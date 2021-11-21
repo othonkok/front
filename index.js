c4692abc53a5fec757b3673bcc27ed30417e1c72
othonkok <45878388+othonkok@users.noreply.github.com>
2021-11-21 15:17:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+//fetch("https://othonmyblog.herokuapp.com")
+fetch("http://localhost:3000/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    let output = "";
+    for (let i = 0; i < 3; i++) {
+      output += `
+        <div class="card" style="width: 30%; min-width:20rem">
+        <div class="card-body">
+          <div class="card-title" style="text-align:center"> ${data[i].title}</div>
+          <div class="card-subtitle" mb-2 text-muted"> ${data[i].date}</div>
+          <div class="card-text mt-4"> ${data[i].description}</div>
+          <a href="#" class="btn btn-primary">Read more</a>
+          </div>
+        </div>`;
+    }
+    document.getElementById("output").innerHTML = output;
+
+    let output2 = "";
+    for (let i = 3; i < data.length; i++) {
+      output2 += `
+        <div class="card" style="width: 20%; min-width:10rem">
+        <div class="card-body">
+          <div class="card-title" style="text-align:center"> ${data[i].title}</div>
+          <div class="card-subtitle" mb-2 text-muted"> ${data[i].date}</div>
+          <div class="card-text mt-4"> ${data[i].description}</div>
+          <a href="#" class="btn btn-primary">Read more</a>
+          </div>
+        </div>`;
+    }
+
+    document.getElementById("output2").innerHTML = output2;
+  });
